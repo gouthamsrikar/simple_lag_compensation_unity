a16af8d2372ed96c5e7913daeb0e60e95f77f37b
gouthamsrikar <53181084+gouthamsrikar@users.noreply.github.com>
2020-10-24 23:13:33 +0530

    Add files via upload

---
+++ clientlag.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clientlag : MonoBehaviour
+{
+    public GameObject server;
+    public float lagtime=0.2f;
+    public float ping=0;
+    public Vector3 position1,rotation1;
+    public Vector3 position2,rotation2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clientprediction(server.transform.position, server.transform.eulerAngles);
+    }
+    void clientprediction(Vector3 position,Vector3 rotation)
+    {
+        ping += Time.deltaTime;
+        if (ping >= lagtime)
+        {
+            changepos(position,rotation);
+            ping = 0;
+            position2 = position1;
+            position1 = position;
+            rotation2 = rotation1;
+            rotation1 = rotation; ;
+        }
+        else
+        {
+            Vector3 fakedistance = (position1 - position2) / lagtime;
+            transform.position += fakedistance * Time.deltaTime;
+            Vector3 fakerotation = (rotation1 - rotation2) / lagtime;
+            transform.eulerAngles += fakerotation * lagtime;
+        }
+    }
+   void changepos(Vector3 position,Vector3 rotation)
+    { 
+        transform.position = position ;
+        transform.eulerAngles =rotation;
+    }
+
+    
+}
